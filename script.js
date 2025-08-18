d941fc38155b90fef9a24c168a3aa114977956e2
Santiago Rodríguez R <63322076+sroco@users.noreply.github.com>
2025-08-18 14:05:07 -0500

    Seed - first files to kick-off project
    one main html and 5 different pages

---
+++ script.js
@@ -0,0 +1,8 @@
+// script.js
+document.getElementById("saludo").addEventListener("click", () => {
+    alert("¡Gracias por visitar mi página!");
+  });
+  
+  <button onclick="window.location.href='https://example.com';">
+  Go to Example
+</button>
