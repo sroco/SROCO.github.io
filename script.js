b6c606dbb63698c9b275f1de70e094c0ee471d59
Santiago Rodríguez R <63322076+sroco@users.noreply.github.com>
2026-06-12 23:03:18 -0500

    worked on the three last pages
    worked further on the form structure and the store display, and created the page "about us" to host the form space
    
    explored and changed the colors in the backgrounds of the site.

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ function showSection(section) {
       break;
 
     case 'portfolio':
-      body.style.background = "#52C5F2";
+      body.style.background = "#355974";
       content.innerHTML = `
         <h2>About me - Santiago Rodríguez</h2>
         <img src="selfie.jpeg" alt="Selfie of Santiago Rodríguez" class="profile-img">
@@ -18,7 +18,7 @@ function showSection(section) {
         the AEC industry and BIM specialist with 5 years of experience delivering residential, healthcare, and hospitality projects. Proficient in BIM 
         coordination and documentation using Revit, Dynamo, and Navisworks, with additional experience in Python for workflow automation 
         and model management. Skilled in multidisciplinary coordination, model QA/QC, and production of construction documentation within 
-        collaborative environments. </p> 
+        collaborative environments. self-place = right</p> 
 
         <p>Experienced working on architectural projects in the United States and Canada, supporting consultant coordination and design 
         development across project phases.
@@ -36,21 +36,29 @@ function showSection(section) {
       `;
       break;
 
-          case 'store':
-      body.style.background = "#508609";
+          case 'about':
+      body.style.background = "#959f89";
       content.innerHTML = `
-        <h2>Revit - BIM</h2>
-        <p>Our expertise in Building Information Modeling (BIM) allows for precision, collaboration, and efficiency.</p>
-        <img src="blueprint.jpg" alt="Revit project">
+        <h2>Hey there!</h2>
+        <p>In ROCO we are please to help you find the best ways to solve any issue or improvement possibility you have in your compay workflow.
+        let us know from you! And we will be happy to help</p>
+        <form>
+        <label>Please tell us your full name</label>
+      <input type = "text" placeholder = "Your name">
+      <label>Please give us an email so we can contact you</label>
+      <input type = "email" placeholder = "Insert your email">
+      <label>Please tell us, wht brought you here? we will be happy to help</label>
+      <textarea placeholder = "What brought you here" textarea.style.textAlign = "left"></textarea>
+      <button type = "submit" class = "market">Send</button>
+      </form>
       `;
       break;
       
     case 'revit':
-      body.style.background = "#4078f1";
+      body.style.background = "#2d2e4e";
       content.innerHTML = `
         <h2>Revit - BIM</h2>
         <p>Our expertise in Building Information Modeling (BIM) allows for precision, collaboration, and efficiency.</p>
-        <img src="blueprint.jpg" alt="Revit project">
       `;
       break;
 
@@ -64,33 +72,33 @@ function showSection(section) {
       break;
 
       case 'PyRevit - Store':
-      body.style.background = "#52e5f2";
+      body.style.background = "#66787a";
       content.innerHTML = `
         <h2>Transform any view type into a drafting view</h2>
         <p></p>
         <img src="Diapositiva1.JPG" alt="Construction work">
         <p></p>
-        <h2>BUY</h2>
+        <button type = "button" class = "market">Buy</button>
         <p></p>
         <h2>Set Detail Numbers Automatically In A Single Sheet</h2>
         <p></p>
         <img src="Diapositiva2.JPG" alt="Construction work">
         <p></p>
-        <h2>BUY</h2>
+        <button type = "button" class = "market">Buy</button>
         <p></p>
         <h2>Name Elevations Automatically</h2>
         <p></p>
         <img src= "Diapositiva3.JPG" alt="Revit project">
         <p></p>
-        <h2>BUY</h2>
+        <button type = "button" class = "market">Buy</button>
         <p></p>
       `;
       break;
 
     case 'bioclimatic':
-      body.style.background = "#5ff252";
+      body.style.background = "#416b3e";
       content.innerHTML = `
-        <h2>Vivienda Bioclimática</h2>
+        <h2>Sustainable housing</h2>
         <p>Bioclimatic housing designed to harmonize with the environment while ensuring comfort and efficiency.</p>
         <img src="PSICOMETRIC-CHART.jpeg" alt="Bioclimatic house">
       `;
